c857e82c3f37e968171257f1729dd71949f3b422
Andrew Diedrich <diedr067@gmail.com>
2020-05-20 17:13:35 -0600

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
+// errros
 export * from './errors/bad-request-error';
 export * from './errors/custom-errors';
 export * from './errors/database-connection-error';
 export * from './errors/not-authorized-error';
 export * from './errors/not-found-error';
 export * from './errors/request-validation-errors';
-
+// middlewares
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
-
+// events
 export * from './events/base-listener';
 export * from './events/base-publisher';
 export * from './events/subjects';
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
+export * from './events/order-cancelled-event';
+export * from './events/order-created-event';
 
 // Types
 export * from './events/types/order-status';

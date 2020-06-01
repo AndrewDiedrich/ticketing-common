96fa2106e7cd5abc0cd3c012521c311bd79193d0
Andrew Diedrich <diedr067@gmail.com>
2020-05-31 18:37:37 -0600

    Updates

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,6 @@ export * from './events/ticket-updated-event';
 export * from './events/order-cancelled-event';
 export * from './events/order-created-event';
 export * from './events/expiration-complete-event';
-
+export * from './events/payments-created-event';
 // Types
 export * from './events/types/order-status';
